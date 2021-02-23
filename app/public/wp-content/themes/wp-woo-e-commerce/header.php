3953ab53ce9a1afbecf4873629392e3835eab970
lukehawkins2205 <l.hawkins2205@outlook.com>
2021-02-23 15:21:22 +0000

    Basic WP Structure
    .

---
+++ app/public/wp-content/themes/wp-woo-e-commerce/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<?php wp_head() ?>
+</head>
+<body <?php body_class('test') ?>>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
